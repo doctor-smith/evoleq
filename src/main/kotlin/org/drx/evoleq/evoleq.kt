c49435d9f685f5c867dd20911409ce910f2939e8
Florian Schmidt <florian.schmidt@platis.de>
2018-11-14 16:44:33 +0100

    Added asyncCoroutine

--- src/main/kotlin/org/drx/evoleq/evoleq.kt
+++ src/main/kotlin/org/drx/evoleq/evoleq.kt
@@ -1,12 +1,13 @@
 package org.drx.evoleq
 
-import kotlinx.coroutines.Deferred
+import javafx.beans.property.SimpleObjectProperty
+import kotlinx.coroutines.*
 
 
 /**
  * Flow of the evolution equation
  */
-tailrec suspend fun <D,T,C > evolve(
+suspend fun <D,T,C > evolve(
     data: D,
     testObject: T,
     condition:(T)->Boolean,
@@ -14,10 +15,52 @@ tailrec suspend fun <D,T,C > evolve(
     flow: (D)->Deferred<D>
     ): D = when ( condition( testObject ) ) {
             false -> data
-            true -> {
-                val newData = flow( data ).await()
-                val newTestObject = updateCondition( newData )
+            true ->  asyncCoroutine {
+                val newData = flow(data).await()
+                val newTestObject = updateCondition(newData)
                 evolve<D, T, C>(newData, newTestObject, condition, updateCondition, flow)
             }
     }
 
+suspend fun <C> asyncCoroutine(block: suspend ()->C): C {
+    val property = SimpleObjectProperty<C>()
+    var updated = false
+    property.addListener{ _, oV, nV ->
+        if(nV != oV) {
+            updated = true
+        }
+    }
+    coroutineScope{launch{
+        property.value = block()
+    }}
+    val result = GlobalScope.async {
+        while(!updated){
+            delay(1)
+        }
+        val result = property.value
+        result
+    }.await()
+    return result
+}
+
+/*
+   var prop = SimpleObjectProperty<D>()
+   var updated = false
+   prop.addListener{_,oV,nV ->
+       if(nV != oV) {
+           updated = true
+       }
+   }
+   coroutineScope{ launch {
+       val newData = flow(data).await()
+       val newTestObject = updateCondition(newData)
+       prop.value = evolve<D, T, C>(newData, newTestObject, condition, updateCondition, flow)
+   }}
+   GlobalScope.async {
+       while(!updated){
+           delay(1)
+       }
+       val result = prop.value
+       result
+   }.await()
+*/
