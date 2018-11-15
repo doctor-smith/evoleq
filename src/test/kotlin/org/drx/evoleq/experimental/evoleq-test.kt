fcd8745b065adb8093ab9cc8bac41dab8e85b29d
Florian Schmidt <florian.schmidt@platis.de>
2018-11-15 14:51:55 +0100

    Updated test

--- src/test/kotlin/org/drx/evoleq/experimental/evoleq-test.kt
+++ src/test/kotlin/org/drx/evoleq/experimental/evoleq-test.kt
@@ -1,6 +1,9 @@
 package org.drx.evoleq.experimental
 
 import kotlinx.coroutines.*
+import org.drx.evoleq.EvolutionConditions
+import org.drx.evoleq.Evolving
+import org.drx.evoleq.Parallel
 import org.drx.evoleq.evolve
 import org.junit.Test
 
@@ -10,21 +13,28 @@ class EvolEqTest{
     @Test
     fun test1()
     {
-        val data = Data(0,0)
-        val testObject = 0
-        val condition: (Int)->Boolean = { x -> x < 10 }
-        val update: (Data)->Int = { newData -> newData.second }
-        val f: (Data)-> Deferred<Data> = { oldData ->
-            GlobalScope.async {
+        val N = 10
+        val data = Data(0, 0)
+        val conditions = EvolutionConditions<Data, Int>(
+            testObject = 0,
+            check = { x -> x <= N },
+            updateCondition = { newData -> newData.second }
+        )
+        val f: (Data) -> Evolving<Data> = { oldData ->
+            Parallel {
                 Thread.sleep(100)
-                val newData =Data(oldData.first + oldData.second, oldData.second +1)
-                println(newData.first)
+                val newData = Data(oldData.first + oldData.second, oldData.second + 1)
+                //println(newData.first)
                 newData
             }
         }
-        GlobalScope.launch {
-            val newData = evolve<Data, Int, Int>(data, testObject, condition, update, f)
+        GlobalScope.async {
+            val newData = evolve(data, conditions) { data ->
+                f(data)
+            }
+            assert(newData.first == N*(N + 1)/2)
         }
+
         Thread.sleep(1_000)
     }
 
