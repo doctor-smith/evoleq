fe076300cacc24e37c3156c19c400f5c8dbfe3da
Florian Schmidt <florian.schmidt@platis.de>
2018-11-23 10:52:59 +0100

    Added special evolution-conditions

--- src/main/kotlin/org/drx/evoleq/conditions.kt
+++ src/main/kotlin/org/drx/evoleq/conditions.kt
@@ -20,3 +20,26 @@ fun<D,T> EvolutionConditions<D,T>.update(data: D): EvolutionConditions<D,T> =
  * Check test-object
  */
 fun<D,T> EvolutionConditions<D,T>.ok(): Boolean = check( testObject )
+
+
+fun <D> once(): EvolutionConditions<D,Boolean> =
+    EvolutionConditions(
+        testObject = true,
+        check = {b->b},
+        updateCondition = {d:D -> false}
+    )
+
+class Counter<D>(private val to: Long) {
+    fun get(): EvolutionConditions<Pair<D, Long>, Long> = EvolutionConditions(
+        testObject = 0L,
+        check= {l -> l < to}
+    ){
+        pair -> pair.second +1L
+    }
+}
+
+fun <D> countable(f: (D)->D): (Pair<D,Long>)->Pair<D,Long> = {
+    pair -> Pair(f(pair.first), pair.second)
+}
+
+
