3be0a6e021d251ce2d44e13897730e0e2bce5133
Florian Schmidt <florian.schmidt@platis.de>
2018-11-24 01:11:32 +0100

    Introduced interface WaitFor

---
+++ src/main/kotlin/org/drx/evoleq/wait.kt
@@ -0,0 +1,25 @@
+package org.drx.evoleq
+
+import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.value.ChangeListener
+import kotlinx.coroutines.*
+
+interface WaitFor<D> {
+    suspend fun toChange(): Evolving<D>
+}
+
+class WaitForProperty<D>(
+    private val property: SimpleObjectProperty<D>,
+    private val delay: Long = 1L
+) : WaitFor<D> {
+    override suspend fun toChange(): Evolving<D> = Parallel {
+        var changed = false
+        val listener = ChangeListener<D> {_,_,nv -> changed = true }
+        property.addListener(listener)
+        while( !changed ){
+            delay( delay )
+        }
+        property.removeListener(listener)
+        property.value
+    }
+}
