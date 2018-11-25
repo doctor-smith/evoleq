e2bd4b3e975ae70935e1e737070b94c21b7c906e
Florian Schmidt <florian.schmidt@platis.de>
2018-11-26 00:05:36 +0100

    Renamed countable to counting

--- src/main/kotlin/org/drx/evoleq/evoleq-impl.kt
+++ src/main/kotlin/org/drx/evoleq/evoleq-impl.kt
@@ -5,13 +5,13 @@ suspend fun<D> repeatParallel(times: Long,initialData: D, f:(D)->D):D =
         initialData = Pair(initialData,0),
         conditions = Counter<D>(times).get()
     ){
-            pair -> Parallel{ countable( f ) (pair) }
+            pair -> Parallel{ counting( f ) (pair) }
     }.first
 suspend fun<D> repeatImmediate(times: Long,initialData: D, f:(D)->D):D =
     evolve<Pair<D,Long>,Long>(
         initialData = Pair(initialData,0),
         conditions = Counter<D>(times).get()
     ){
-            pair -> Immediate{ countable( f ) (pair) }
+            pair -> Immediate{ counting( f ) (pair) }
     }.first
 
