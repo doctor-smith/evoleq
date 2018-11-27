6a8914c0982985756abce211b305b6a060e17cb3
Florian Schmidt <florian.schmidt@platis.de>
2018-11-27 09:34:33 +0100

    Moved test, added test-skeleton to test wether immediate breaks parallel

--- src/test/kotlin/org/drx/evoleq/evolving-test.kt
+++ src/test/kotlin/org/drx/evoleq/evolving-test.kt
@@ -1,14 +1,15 @@
 package org.drx.evoleq
 
 import javafx.beans.property.SimpleObjectProperty
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.Test
-import java.lang.Exception
 
 class EvolvingTest {
+
+    @Test
+    fun immediateDoesNotBreakParallel() = runBlocking{
+        /* TODO find good test */
+    }
 
     @Test
     fun testTimes() {
@@ -67,47 +68,5 @@ class EvolvingTest {
     @Test
     fun testBackPropagationSndAttempt() = runBlocking {
 
-        data class P(val message: String)
-        data class W(val p: P)
-
-        val from = { w: W -> Immediate { w.p } }
-        val to = { w: W -> { p: P -> Immediate { w.copy(p = p) } } }
-        val gap: Gap<W, P> = Gap(from, to)
-
-        class SideEffect(gap: Gap<W, P>) : Spatula<W, P> {
-            val fProp =  SimpleObjectProperty<(W)->Evolving<W>>()
-            var fSet = false
-            init{Parallel{
-                println("init")
-                fProp.value = fill(gap)
-                fSet = true
-            }}
-            override suspend fun fill(gap: Gap<W, P>): (W) -> Evolving<W> {
-                val filler: (P) -> Evolving<P> = { p ->
-                    Parallel {
-                        println("Wait ...")
-                        delay(1000)
-                        println("Message is: ${p.message}")
-
-
-                        P("Ok")
-                    }
-                }
-                val f = gap.fill ( filler )
-                return f
-            }
-            fun print(w:W): Evolving<W> = Parallel {
-                while(!fSet){
-                    delay(1)
-                }
-                val f = fProp.value
-                f(w).get()
-
-            }
-        }
-
-        val se = SideEffect(gap)
-        val p = se.print(W(P("nice message"))).get().p
-        println(p.message)
     }
 }
