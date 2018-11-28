523f023c3e82271f5815bc620941250b17df04dc
Florian Schmidt <florian.schmidt@platis.de>
2018-11-28 06:48:04 +0100

    Moved code

---
+++ src/main/kotlin/org/drx/evoleq/gap-impl.kt
@@ -0,0 +1,5 @@
+package org.drx.evoleq
+
+fun unitGap(): Gap<Unit,Unit> = Gap( {u:Unit->Immediate{u}}, {u:Unit -> {v:Unit -> Immediate{v}}} )
+fun <D> initGap(d:D):Gap<Unit,D> = Gap({Immediate{d}}, {{d:D->Immediate{}}})
+fun <D> selfGap():Gap<D,D> = Gap({d:D->Immediate{d}}, {d:D->{e:D->Immediate{e}}})
