8da45744dc191060077e9a45611b50d40a98cc4e
Florian Schmidt <florian.schmidt@platis.de>
2018-12-06 21:37:52 +0100

    Added coroutine playground

---
+++ src/test/kotlin/org/drx/evoleq/coroutine-playground.kt
@@ -0,0 +1,32 @@
+package org.drx.evoleq
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class CoroutinePlayground {
+    @Test
+    fun exit() = runBlocking{
+        val x = {
+
+            var z = 0
+            GlobalScope.launch exit@{
+                z = 5
+                return@exit
+            }
+            println(z)
+        }
+    }
+    @Test
+    fun exitFun() {
+        exit@ fun<T,D> T.exit(block:T.()->D): D = this.block()
+        var y = exit@{
+            val N = 3.0
+            return@exit N
+            Unit
+
+
+        }
+    }
+}
