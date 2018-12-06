e5f397a829b99d46a2731284ac5781c12398bd69
Florian Schmidt <florian.schmidt@platis.de>
2018-12-06 21:36:41 +0100

    Removed obsolete code

--- src/main/kotlin/org/drx/evoleq/util.kt
+++ src/main/kotlin/org/drx/evoleq/util.kt
@@ -4,10 +4,8 @@ fun <T> ArrayList<T>.tail(): ArrayList<T> {
     val N = size
 
     if(N <= 1){
-        //println("tail(): size = ${N-1}")
         return arrayListOf()
     }
-    //println("tail(): size = ${N-1}")
     val tail = arrayListOf<T>()
     IntRange(1,N-1).forEach { tail.add(this[it]) }
     return tail
