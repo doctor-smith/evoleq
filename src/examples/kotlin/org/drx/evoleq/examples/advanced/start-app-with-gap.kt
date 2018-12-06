a42638175f0d97223250b8b812fc1348c98981fd
Florian Schmidt <florian.schmidt@platis.de>
2018-12-06 21:37:17 +0100

    Started implementing example

---
+++ src/examples/kotlin/org/drx/evoleq/examples/advanced/start-app-with-gap.kt
@@ -0,0 +1,23 @@
+package org.drx.evoleq.examples.advanced
+
+import org.drx.evoleq.*
+
+interface AppStub {
+
+}
+
+data class Data(val preData: PreData, val appStub: AppStub)
+data class PreData(val data: String)
+
+val mainGap = Gap({data: Data -> Immediate{data.preData} },{data -> {pd -> Immediate{data.copy(preData = pd)}}})
+
+val appGap = Gap({appStub:AppStub -> Immediate{appStub} }, { appStub: AppStub ->{_ -> Immediate{appStub}}  })
+
+class App(gap: Gap<AppStub,()->AppStub>) : tornadofx.App() {
+
+    init{
+
+    }
+
+
+}
