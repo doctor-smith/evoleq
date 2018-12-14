fd285f1698faaaa3361516b1835b1eac7ad859c7
Florian Schmidt <florian.schmidt@platis.de>
2018-12-14 08:02:05 +0100

    removed code

--- src/examples/kotlin/org/drx/evoleq/examples/advanced/start-app-with-gap.kt
+++ src/examples/kotlin/org/drx/evoleq/examples/advanced/start-app-with-gap.kt
@@ -3,24 +3,3 @@ package org.drx.evoleq.examples.advanced
 import org.drx.evoleq.evolving.Immediate
 import org.drx.evoleq.gap.Gap
 
-interface AppStub {
-
-}
-
-data class Data(val preData: PreData, val appStub: AppStub)
-data class PreData(val data: String)
-
-val mainGap = Gap({ data: Data -> Immediate { data.preData } },
-    { data -> { pd -> Immediate { data.copy(preData = pd) } } })
-
-val appGap = Gap({ appStub: AppStub -> Immediate { appStub } },
-    { appStub: AppStub -> { _ -> Immediate { appStub } } })
-
-class App(gap: Gap<AppStub, () -> AppStub>) : tornadofx.App() {
-
-    init{
-
-    }
-
-
-}
