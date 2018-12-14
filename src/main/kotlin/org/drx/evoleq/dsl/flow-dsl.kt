2e4d387bd21b6ff7951ee36492288a86fc0d7e8f
Florian Schmidt <florian.schmidt@platis.de>
2018-12-14 07:58:55 +0100

    No constructor args in dsl

--- src/main/kotlin/org/drx/evoleq/dsl/flow-dsl.kt
+++ src/main/kotlin/org/drx/evoleq/dsl/flow-dsl.kt
@@ -5,10 +5,11 @@ import org.drx.evoleq.evolving.Evolving
 import org.drx.evoleq.flow.Flow
 import org.drx.evoleq.flow.SuspendedFlow
 
-class FlowConfiguration<D,T>(
-    var conditions: EvolutionConditions<D, T>?,
-    var flow: ((D)-> Evolving<D>)?
-) : Configuration<Flow<D, T>> {
+class FlowConfiguration<D,T> : Configuration<Flow<D, T>> {
+
+    var conditions: EvolutionConditions<D, T>? = null
+    var flow: ((D)-> Evolving<D>)? = null
+
     override fun configure(): Flow<D, T> = Flow(conditions!!, flow!!)
 }
 fun <D,T> flow(configure: FlowConfiguration<D,T>.()->Unit) : Flow<D, T> = org.drx.evoleq.dsl.configure(configure)
--- src/main/kotlin/org/drx/evoleq/dsl/suspended-flow-dsl.kt
+++ src/main/kotlin/org/drx/evoleq/dsl/suspended-flow-dsl.kt
@@ -5,10 +5,11 @@ import org.drx.evoleq.evolving.Evolving
 import org.drx.evoleq.flow.SuspendedFlow
 
 
-class SuspendedFlowConfiguration<D,T>(
-    var conditions: EvolutionConditions<D, T>?,
-    var flow: (suspend (D)-> Evolving<D>)?
-) : Configuration<SuspendedFlow<D, T>> {
+class SuspendedFlowConfiguration<D,T> : Configuration<SuspendedFlow<D, T>> {
+
+    var conditions: EvolutionConditions<D, T>? = null
+    var flow: (suspend (D)-> Evolving<D>)? = null
+
     override fun configure(): SuspendedFlow<D, T> = SuspendedFlow(conditions!!, flow!!)
 }
-fun <D,T> suspendedFlow(configure: SuspendedFlowConfiguration<D,T>.()->Unit) : SuspendedFlow<D, T> = org.drx.evoleq.dsl.configure(configure)
+fun <D,T> suspendedFlow(configure: SuspendedFlowConfiguration<D,T>.()->Unit) : SuspendedFlow<D, T> = configure(configure)
