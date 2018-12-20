d8cdc76f4b2f7ba11d3e013e16e41dfa5ce9df1f
Florian Schmidt <florian.schmidt@platis.de>
2018-12-20 18:40:11 +0100

    Added sub stubs to stub interface

--- src/examples/kotlin/org/drx/evoleq/examples/application/stub.kt
+++ src/examples/kotlin/org/drx/evoleq/examples/application/stub.kt
@@ -2,12 +2,16 @@ package org.drx.evoleq.examples.application
 
 import org.drx.evoleq.evolving.Evolving
 import org.drx.evoleq.evolving.Immediate
+import kotlin.reflect.KClass
 
 interface Stub<D> {
     suspend fun stub(d: D): Evolving<D>
+    fun stubs(): HashMap<KClass<*>, Stub<*>>
 }
 class InitStub<D> : Stub<D>{
     override suspend fun stub(d: D): Evolving<D> {
         return Immediate{d}
     }
+    val stubs: HashMap<KClass<*>, Stub<*>> by lazy{ HashMap<KClass<*>, Stub<*>>() }
+    override fun stubs(): HashMap<KClass<*>, Stub<*>> = stubs
 }
