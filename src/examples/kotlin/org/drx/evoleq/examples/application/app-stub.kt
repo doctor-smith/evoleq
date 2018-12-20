dbf7c959aa98c702ccdb54d32a27f31adcdef9a6
Florian Schmidt <florian.schmidt@platis.de>
2018-12-20 18:44:08 +0100

    sub stubs

--- src/examples/kotlin/org/drx/evoleq/examples/application/app-stub.kt
+++ src/examples/kotlin/org/drx/evoleq/examples/application/app-stub.kt
@@ -2,10 +2,15 @@ package org.drx.evoleq.examples.application
 
 import org.drx.evoleq.evolving.Evolving
 import org.drx.evoleq.evolving.Immediate
+import kotlin.reflect.KClass
 
 interface ApplicationStub<D>: Stub<D>
 
 class InitAppStub<D> : ApplicationStub<D> {
+
+    val stubs: HashMap<KClass<*>, Stub<*>> by lazy{ HashMap<KClass<*>, Stub<*>>() }
+    override fun stubs(): HashMap<KClass<*>, Stub<*>> = stubs
+
     override suspend fun stub(d: D): Evolving<D> {
         return Immediate{d}
     }
--- src/examples/kotlin/org/drx/evoleq/examples/application/dsl/app-stub-dsl.kt
+++ src/examples/kotlin/org/drx/evoleq/examples/application/dsl/app-stub-dsl.kt
@@ -4,12 +4,16 @@ import org.drx.evoleq.dsl.Configuration
 import org.drx.evoleq.evolving.Evolving
 import org.drx.evoleq.evolving.Immediate
 import org.drx.evoleq.examples.application.ApplicationStub
+import org.drx.evoleq.examples.application.Stub
+import kotlin.reflect.KClass
 
 class ApplicationStubConfiguration<D> : Configuration<ApplicationStub<D>>{
-    var stub:suspend (D)-> Evolving<D> = {d -> Immediate{d} }
+    var stub: suspend (D)-> Evolving<D> = {d -> Immediate{d} }
 
     override fun configure(): ApplicationStub<D> = object: ApplicationStub<D> {
         override suspend fun stub(d: D): Evolving<D> = this@ApplicationStubConfiguration.stub(d)
+        val stubs: HashMap<KClass<*>, Stub<*>> by lazy{ HashMap<KClass<*>, Stub<*>>() }
+        override fun stubs(): HashMap<KClass<*>, Stub<*>> = stubs
     }
 }
 
