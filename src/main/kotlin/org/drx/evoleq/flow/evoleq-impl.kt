d292632625ac649175510313f1e370879c224c48
Florian Schmidt <florian.schmidt@platis.de>
2018-12-21 17:28:53 +0100

    Clean up

--- src/main/kotlin/org/drx/evoleq/flow/evoleq-impl.kt
+++ src/main/kotlin/org/drx/evoleq/flow/evoleq-impl.kt
@@ -25,14 +25,14 @@ suspend fun<D> repeatParallel(times: Long,initialData: D, f:(D)->D):D =
     evolve<Pair<D, Long>, Long>(
         initialData = Pair(initialData, 0),
         conditions = Counter<D>(times).get()
-    ) { pair ->
-        Parallel { counting(f)(pair) }
+    ) {
+        pair -> Parallel { counting(f)(pair) }
     }.first
 suspend fun<D> repeatImmediate(times: Long,initialData: D, f:(D)->D):D =
     evolve<Pair<D, Long>, Long>(
         initialData = Pair(initialData, 0),
         conditions = Counter<D>(times).get()
-    ) { pair ->
-        Immediate { counting(f)(pair) }
+    ) {
+        pair -> Immediate { counting(f)(pair) }
     }.first
 
