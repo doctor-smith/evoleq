6bf5591bf6b871bd587929655a6d28fea72f824f
Florian Schmidt <florian.schmidt@platis.de>
2019-02-11 22:34:43 +0100

    observing stub dsl

--- src/main/kotlin/org/drx/evoleq/stub/stub.kt
+++ src/main/kotlin/org/drx/evoleq/stub/stub.kt
@@ -23,17 +23,16 @@ import org.drx.evoleq.flow.Evolver
 import org.drx.evoleq.flow.SuspendedFlow
 import kotlin.reflect.KClass
 
+class DefaultIdentificationKey
 interface Stub<D> : Evolver<D> {
+
     override suspend fun evolve(d: D): Evolving<D> = Immediate{ d }
 
+    val id: KClass<*>
     val stubs: HashMap<KClass<*>, Stub<*>>
 }
 class ParentStubKey
 fun <D,T> Stub<D>.toFlow(conditions: EvolutionConditions<D,T>): SuspendedFlow<D,T> = suspendedFlow {
     conditions(conditions)
     flow{ d -> this@toFlow.evolve(d) }
-}
-
-open class InitStub<D> : Stub<D>{
-    override val stubs: HashMap<KClass<*>, Stub<*>> by lazy{ HashMap<KClass<*>, Stub<*>>() }
 }
--- src/test/kotlin/org/drx/evoleq/stub/stub-test.kt
+++ src/test/kotlin/org/drx/evoleq/stub/stub-test.kt
@@ -15,13 +15,16 @@
  */
 package org.drx.evoleq.stub
 
-import javafx.beans.property.IntegerProperty
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleObjectProperty
 import kotlinx.coroutines.runBlocking
 import org.drx.evoleq.conditions.once
-import org.drx.evoleq.dsl.stub
+import org.drx.evoleq.dsl.*
 import org.drx.evoleq.evolving.Immediate
+import org.drx.evoleq.evolving.Parallel
+import org.drx.evoleq.message.Message
 import org.junit.Test
+import java.lang.Thread.sleep
 
 class StubTest {
 
@@ -67,4 +70,280 @@ class StubTest {
         }
     }
 
+    @Test
+    fun observingStub() {
+        val prop = SimpleObjectProperty<String>()
+        val change = SimpleObjectProperty<String>()
+        val stub = observingStub<Int,String>{
+            observe(prop)
+
+            evolve { x -> Immediate{
+                x
+            }}
+            gap{
+                from{ x -> Immediate{ "$x" } }
+                to{ x , y -> Immediate{
+                    change.value = "$x"+y
+                    ("$x"+y).length
+                }}
+            }
+        }
+        runBlocking {
+            val x = stub.evolve(8)
+
+            prop.value = "supi"
+
+            val y = x.get()
+            //sleep(1_000)
+            assert(change.value == "8supi")
+            assert(y == 5)
+        }
+    }
+    @Test
+    fun observingStubAsFlow() {
+        class Data(val x: Int, val y: String, val clientId: Int)
+        class Request(val message: String, val clientId: Int)
+        val property = SimpleObjectProperty<Request>()
+        val observingStub = observingStub<Data,Request> {
+            gap{
+                from {data -> Immediate{Request("waiting",data.x)}}
+                to{data, request -> Immediate{
+                    Data(data.x+1, request.message, request.clientId)}
+                }
+            }
+            observe(property)
+        }
+        val callingStub = stub<Data>{
+            evolve{ data -> observingStub.evolve(data) }
+        }
+        val flow = callingStub.toFlow<Data,Boolean>(
+            conditions{
+                testObject(true)
+                check { b -> b }
+                updateCondition { data: Data -> data.x < 1000}
+            }
+        )
+        runBlocking {
+            sleep(1_000)  // make sure that flow is configured before firing data
+            val result  = flow.evolve(Data(0, "",-1))
+            IntRange(1,11).forEach {i ->
+                //GlobalScope.launch {
+                Parallel{
+                    IntRange(1, 101).forEach {
+                        property.value = Request("$it", i)
+                        sleep(1)
+                    }
+                }
+
+            }
+            assert(result.get().x >= 950)
+
+        }
+    }
+
+    @Test
+    fun pauseObservingStub() {
+        class Data(val x: Int, val cnt: Int = 0)
+        class ObserverKey
+        class OutKey
+        class ChangeBehaviorKey
+        class ObserveBehaviorKey
+        val observe = SimpleObjectProperty<Boolean>(true)
+
+        val observedProperty = SimpleObjectProperty<Int>()
+
+        var launched = false
+
+        val stub = stub<Data> {
+            // make observer accessible to child processes
+            // Clearly it is possible to access Observer directly via
+            // fun observer() : child() ObserverKey::class
+            // but then it is callable by all its siblings
+
+            fun observer() = sibling(ChangeBehaviorKey::class,ObserverKey::class)!! as Stub<Int>
+
+
+            evolve{ data -> Immediate{
+                val observer = child(ObserverKey::class) as Stub<Int>
+                    if(!launched) {
+                        launched = true
+                        val behavior = child(ObserveBehaviorKey::class) as Stub<Boolean>
+                        Parallel{val res =behavior.toFlow<Boolean,Boolean>(
+                            conditions{
+                                testObject(true)
+                                check{b -> b}
+                                updateCondition { b ->true }
+                            }
+                        ).evolve(true).get()
+                        }
+
+                    }
+                    val res = observer.evolve(data.x).get()
+                    Data(res, data.cnt+1)
+            }}
+            // parental stubs
+            // prints received value to the console
+            parentalStub(
+                OutKey::class,
+                stub<String>{
+                    evolve{s-> Immediate{
+                        println(s)
+                        s
+                    }}
+                }
+            )
+            // Updates the observation behavior
+            parentalStub(
+                ChangeBehaviorKey::class,
+                stub<Boolean> {
+                    evolve{ b -> Immediate{
+                        val observer = observer()//child(ObserverKey::class) as Stub<Int>
+                        val obs = observer.stubs[ObservePropertyStub::class]!! as Stub<ObservePropertyMessage>
+                        var o: Message? = null
+                        if(b){
+                            println("observe")
+                            o =  obs.evolve(ObservePropertyMessage.Observe).get()
+                        }
+                        else{
+                            println("pause")
+                            o = obs.evolve(ObservePropertyMessage.Pause).get()
+                        }
+                        b
+                    }}
+                }
+            )
+
+            // children
+            // Process observing the observed property
+            child( observingStub<Int,Int>{
+                    id ( ObserverKey::class )
+                    gap{
+                        from{ x -> Immediate{ x } }
+                        to{x,y -> Immediate{
+                            val parentStub = parent() as Stub<String>
+                            val res = parentStub.evolve("received: $y").get()
+                            y
+                        }}
+                    }
+                    observe(observedProperty)
+                },
+                OutKey::class
+
+            )
+
+            // Process looking for changes of the observe property.
+            child( ObserveBehaviorKey::class,
+                observingStub<Boolean,Boolean>{
+                    gap{
+                        from{ x -> Immediate{ x } }
+                        to{x,y -> Immediate{
+                            val parentStub = parent() as Stub<Boolean>
+                            val res = parentStub.evolve(y).get()
+                            y
+                        }}
+                    }
+                    observe(observe)
+                },
+                ChangeBehaviorKey::class
+            )
+            // siblings access
+            ChangeBehaviorKey::class.grantAccessToSiblings(ObserverKey::class)
+        }
+
+        // Turn stub into flow
+        val flow = stub.toFlow(
+            conditions<Data, Boolean>{
+                testObject(true)
+                check{b -> b}
+                updateCondition { data ->  data.cnt <= 2 }
+            }
+        )
+
+        runBlocking{
+            sleep(1_000)
+            var done = false
+            Parallel {
+                val result = flow.evolve(Data(0))
+                val res = result.get().x
+                assert (res == 4)
+                println(res)
+                done = true
+
+            }
+            observe.value = false
+            sleep(1_000)
+            observedProperty.value = 1
+
+            observe.value = true
+            sleep(1_000)
+            observedProperty.value = 2
+            observedProperty.value = 3
+            observedProperty.value = 4
+            observe.value = false
+            observedProperty.value = 5
+            while(!done){
+                sleep(10)
+            }
+
+        }
+    }
+
+    @Test
+    fun racing() {
+        class Data(val x : Int = 0, val data: String = "")
+        open class Result(val value: String = "")
+
+        class First
+        class Second
+
+        val wrapperStub = observingStub<Data, Result>{
+
+            val property = SimpleObjectProperty<Result>()
+            observe(property)
+
+
+            gap{
+                from{data -> Immediate{Result(data.data)}}
+                to{data, result -> Immediate{Data(data.x,result.value)}}
+            }
+            child(First::class,
+                stub<Result>{
+                    evolve{ it -> Parallel{
+                        sleep(50)
+                        val res = Result("first")
+                        property.value = res
+                        res
+                    }
+                    }
+                }
+            )
+            child(Second::class,
+                stub<Result>{
+                    evolve{  it -> Parallel{
+                        sleep(10)
+                        val res = Result("second")
+                        property.value = res
+                        res
+                        }
+                    }
+                }
+            )
+
+
+            evolve{
+                data -> Immediate{
+                    val res1 = (child(First::class) as Stub<Result>).evolve(Result())
+                    val res2 = (child(Second::class )as Stub<Result>).evolve(Result())
+                    data
+                }
+            }
+        }
+
+        runBlocking{
+            sleep(1_000) // make sure that configuration has taken place
+            val res = wrapperStub.evolve(Data()).get()
+            assert(res.data == "second")
+        }
+
+    }
 }
