50a73e551fa462c53befd94087f54d12d989b415
Florian Schmidt <florian.schmidt@platis.de>
2019-04-15 23:22:28 +0200

    Code cosmetics

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -35,33 +35,12 @@ object Config {
         val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
         val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
 
-        val pipeline = "org.drx:pipeline-kt:${Versions.pipeline}"
-
-        val jfxmobile = "org.javafxports:jfxmobile-plugin:${Versions.jfxmobile}"
-        val retrolambda = "gradle.plugin.me.tatarka:gradle-retrolambda:${Versions.retrolambda}"
-        val orfjacklRetrolambdaConfig = "net.orfjackal.retrolambda:retrolambda:+"
 
         val tornadofx  = "no.tornado:tornadofx:${Versions.tornadofx}"
-        val tornadofxAndroidCompat = "no.tornado:tornadofx-android-compat:${Versions.tornadofxAndroidCompat}"
-
-        val junit = "junit:junit:${Versions.junit}"
-        val grpcTesting = "io.grpc:grpc-testing:${Config.Versions.grpc}"
-
-        val androidToolsGradle = "com.android.tools.build:gradle:${Versions.androidPlugin}"
-    }
-
-    object Repositories {
-        val drxMavenSnapshots = "http://drx.maven.net:8081/repository/snapshots"
-
-        val drxBintray = "https://dl.bintray.com/drx/maven"
-
-        val gradlePluginsM2 = "https://plugins.gradle.org/m2/"
-
-        val jitpackIo = "https://jitpack.io"
-
-        val googleMvn = "https://maven.google.com"
-
 
     }
+
+
+
 
 }
