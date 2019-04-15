ada3b44f020db549887386908694b77f683a64f1
Florian Schmidt <florian.schmidt@platis.de>
2019-04-15 23:23:55 +0200

    Code cosmetics

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -11,19 +11,8 @@ object Config {
         val kotlin = "1.3.20"
         val coroutines = "1.1.1"
 
-        val pipeline = "1.0.4"
-
-        val jfxmobile = "1.3.11"
-        val retrolambda = "3.7.0" //"+"
-        val javafxports = "8.60.9"
 
         val tornadofx  = "1.7.18"
-        val tornadofxAndroidCompat = "1.0.1"
-
-        val grpc = "1.15.1"
-        val protobuf = "0.8.6"
-
-        val androidPlugin = "3.2.1"
 
         val junit = "4.12"
 
