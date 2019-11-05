9b550da1db3ee01666608dd1ac841835f40c35eb
Florian Schmidt <florian.schmidt@platis.de>
2019-11-05 22:05:08 +0100

    Remove unused dependencies

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -21,19 +21,12 @@ object Config {
         val kotlin = "1.3.50"
         val coroutines = "1.3.1"
 
-
-        val tornadofx  = "1.7.18"
-
         val junit = "4.12"
     }
 
     object Dependencies {
         val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
         val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
-
-
-        val tornadofx  = "no.tornado:tornadofx:${Versions.tornadofx}"
-
     }
 
 
